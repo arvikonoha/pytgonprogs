26cfb9c539e2afe5fbafe53af73273ec41797e87
arvi <arvikonoha@gmail.com>
2019-10-21 04:27:48 +0530

    Fifth commit

--- student.py
+++ student.py
@@ -22,6 +22,10 @@ curser.execute("""
 
 window = Tk()
 
+nameT = StringVar()  # newline
+branchT = StringVar()  # newline
+ageT = StringVar()  # newline
+
 window.geometry("300x300")
 usn_label = Label(window, text="USN")
 usn_label.grid(column=0, row=0)
@@ -29,34 +33,16 @@ usn_text = Entry(window, width=10)
 usn_text.grid(column=1, row=0)
 name_label = Label(window, text="NAME")
 name_label.grid(column=0, row=1)
-name_text = Entry(window, width=10)
+name_text = Entry(window, width=10, textvariable=nameT)  # newline
 name_text.grid(column=1, row=1)
 age_label = Label(window, text="AGE")
 age_label.grid(column=0, row=2)
-age_text = Entry(window, width=10)
+age_text = Entry(window, width=10, textvariable=ageT)  # newline
 age_text.grid(column=1, row=2)
 branch_label = Label(window, text="BRANCH")
 branch_label.grid(column=0, row=3)
-branch_text = Entry(window, width=10)
+branch_text = Entry(window, width=10, textvariable=branchT)  # newline
 branch_text.grid(column=1, row=3)
-
-
-def insert():
-    usn = usn_text.get()
-    name = name_text.get()
-    age = age_text.get()
-    branch = branch_text.get()
-    if len(usn) != 0 and len(name) != 0 and len(age) != 0 and len(branch) != 0:
-        try:
-            curser.execute(
-                f"INSERT INTO STUDENT VALUES({usn},'{name}',{age},'{branch}')")
-            db.commit()
-            messagebox.showinfo("Insert", "INSERTION SUCCESSFUL")
-        except:
-            db.rollback()
-            messagebox.showinfo("error", "INSERTION FAILED")
-    else:
-        messagebox.showinfo("PLEASE ENTER ALL THE FIELDS")
 
 
 def search():
@@ -66,10 +52,10 @@ def search():
             curser.execute(f"SELECT * FROM STUDENT WHERE USN={usn}")
             if (curser.rowcount != 0):
                 row = curser.fetchone()
-                messagebox.showinfo(
-                    "Insert",
-                    f"USN : {row[0]}\nNAME : {row[1]}\nAGE : {row[2]}\nBRANCH : {row[3]}"
-                )
+                messagebox.showinfo("success", "Record obtained")
+                nameT.set(row[1])
+                ageT.set(row[2])
+                branchT(row[3])
             else:
                 messagebox.showinfo("error", "THERE IS NO SUCH RECORD")
         except:
@@ -78,8 +64,6 @@ def search():
         messagebox.showinfo("PLEASE ENTER USN")
 
 
-insert = Button(window, text="INSERT STUDENT", command=insert)
-insert.grid(column=0, row=4)
 search = Button(window, text="SEARCH BY USN", command=search)
 search.grid(column=1, row=4)
 window.mainloop()
